5125f4da7bbe74073d6fd419b833e509f70f8ea6
Rubén Gómez <rubengomez@paucasesnovescifp.cat>
2025-12-07 13:28:41 +0100

    Creacion del componente de test

---
+++ src/main/java/com/gomez/yourdownloadpoller/YourDownloadPoller.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gomez.yourdownloadpoller;
+
+/**
+ *
+ * @author LionKeriot
+ */
+public class YourDownloadPoller {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
